3070b2b6d7ca3d9d3d9d8f4a5de7a0295613ec83
Harrison <djor15382@gmail.com>
2024-06-07 19:13:26 -0700

    sh

--- lib/main.dart
+++ lib/main.dart
@@ -140,6 +140,7 @@ class CustomLayout extends StatelessWidget {
                     color: Color(0xFF999999),
                     fontSize: 10,
                     fontFamily: 'PingFang SC',
+                    
                     fontWeight: FontWeight.w400,
                     height: 2.0,
                   ),
